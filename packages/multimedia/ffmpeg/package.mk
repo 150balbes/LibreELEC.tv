f0176b94cf035115f72915b1e9c2d2ce4692531e
chlorophyll-zz <43696399+chlorophyll-zz@users.noreply.github.com>
2019-03-10 11:25:10 +0100

    compile rockchip-5.0 without --enable-v4l2-request
    if $LINUX=rockchip-5.0, ffmpeg build fails with
    Unknown option "--enable-v4l2-request".
    See ./configure --help for available options.
    FAILURE: scripts/install ffmpeg has failed!

--- packages/multimedia/ffmpeg/package.mk
+++ packages/multimedia/ffmpeg/package.mk
@@ -37,10 +37,10 @@ else
   PKG_FFMPEG_VDPAU="--disable-vdpau"
 fi
 
-if [ "$PROJECT" = "Rockchip" -a "$LINUX" = "rockchip-4.4" ]; then
+if [ "$PROJECT" = "Rockchip" -a "$LINUX" = "rockchip-4.4"|"rockchip-5.0" ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET rkmpp"
   PKG_FFMPEG_RKMPP="--enable-rkmpp --enable-libdrm --enable-version3"
-elif [ "$PROJECT" = "Rockchip" -a "$LINUX" != "rockchip-4.4" ]; then
+elif [ "$PROJECT" = "Rockchip" -a "$LINUX" != "rockchip-4.4"|"rockchip-5.0" ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET libdrm"
   PKG_FFMPEG_RKMPP="--disable-rkmpp --enable-v4l2-request --enable-libdrm"
 else
